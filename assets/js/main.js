b78af8c8e3654e75956ed5d61e11bddd47047f0c
bhavikt5 <91686940+Bhavikt5@users.noreply.github.com>
2023-04-20 21:30:02 +0530

    file added

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$.get("components/header.html", function (header_var) {
+  $("#header").html(header_var);
+});
+
+$.get("components/footer.html", function (header_var) {
+  $("#footer").html(header_var);
+});
